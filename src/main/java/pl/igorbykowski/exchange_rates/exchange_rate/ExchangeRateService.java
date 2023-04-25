e59c54b5b4d48bd9cc2b5f133fe71e6c30a4f099
lyingparachute <igor1501@o2.pl>
2023-04-25 09:15:13 +0200

    Update getAverageExchangeRateByDateAndCurrency method

--- src/main/java/pl/igorbykowski/exchange_rates/exchange_rate/ExchangeRateService.java
+++ src/main/java/pl/igorbykowski/exchange_rates/exchange_rate/ExchangeRateService.java
@@ -6,8 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.igorbykowski.exchange_rates.currency.Currency;
-import pl.igorbykowski.exchange_rates.exchange_rate.NBP_api_response.ExchangeRateNBPResponse;
-import pl.igorbykowski.exchange_rates.exchange_rate.NBP_api_response.RateNBPResponse;
+import pl.igorbykowski.exchange_rates.exchange_rate.nbp_api_response.ExchangeRateNBPResponse;
+import pl.igorbykowski.exchange_rates.exchange_rate.nbp_api_response.RateNBPResponse;
 import pl.igorbykowski.exchange_rates.exchange_rate.average.AverageExchangeRateResponse;
 import pl.igorbykowski.exchange_rates.exchange_rate.difference.BidAskDifferenceResponse;
 import pl.igorbykowski.exchange_rates.exchange_rate.min_max.MinMaxAverageValueResponse;
@@ -15,10 +15,7 @@ import pl.igorbykowski.exchange_rates.exchange_rate.min_max.MinMaxAverageValueRe
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,11 +25,13 @@ public class ExchangeRateService {
 
     public AverageExchangeRateResponse getAverageExchangeRateByDateAndCurrency(String currencyCode, LocalDate date) {
         String table = "A";
-        String url = String.format(NBP_API_URL, table, getCurrencyCode(currencyCode), getDateString(date));
-        ResponseEntity<AverageExchangeRateResponse> response = restTemplate.exchange(url, HttpMethod.GET, null,
+        Currency currency = getCurrencyCode(currencyCode);
+        String url = String.format(NBP_API_URL, table, currency, getDateString(date));
+        ResponseEntity<ExchangeRateNBPResponse> response = restTemplate.exchange(url, HttpMethod.GET, null,
                 new ParameterizedTypeReference<>() {
                 });
-        return response.getBody();
+        BigDecimal averageExchangeRate = Objects.requireNonNull(response.getBody()).rates().get(0).mid();
+        return new AverageExchangeRateResponse(currency, currency.getDescription(), date, averageExchangeRate);
     }
 
     public MinMaxAverageValueResponse getMinMaxAverageValue(String currencyCode, int topCount) {
@@ -54,21 +53,6 @@ public class ExchangeRateService {
         return new MinMaxAverageValueResponse(currency, minValue, maxValue);
     }
 
-    private String getDateString(LocalDate date) {
-        return date.format(DateTimeFormatter.ISO_LOCAL_DATE);
-    }
-
-    private Currency getCurrencyCode(String code) {
-        if (isValidCurrencyCode(code))
-            return Currency.valueOf(code);
-        else throw new IllegalArgumentException("Wrong currencyCode currencyCode");
-    }
-
-    private boolean isValidCurrencyCode(String code) {
-        return Arrays.stream(Currency.values())
-                .anyMatch(c -> c.name().equals(code));
-    }
-
     public BidAskDifferenceResponse getMajorDifferenceBetweenBuyAndAskRate(String currencyCode, int quotations) {
         String table = "C";
         Currency currency = getCurrencyCode(currencyCode);
@@ -86,6 +70,21 @@ public class ExchangeRateService {
                 .max(Map.Entry.comparingByValue())
                 .get();
         return new BidAskDifferenceResponse(currency, biggestDifference.getKey(), biggestDifference.getValue());
+    }
+
+    private String getDateString(LocalDate date) {
+        return date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+
+    private Currency getCurrencyCode(String code) {
+        if (isValidCurrencyCode(code))
+            return Currency.valueOf(code);
+        else throw new IllegalArgumentException("Wrong currencyCode currencyCode");
+    }
+
+    private boolean isValidCurrencyCode(String code) {
+        return Arrays.stream(Currency.values())
+                .anyMatch(c -> c.name().equals(code));
     }
 }
 
